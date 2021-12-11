33ef330f3bb3c7ebab2b6ec8071f19f490df708f
Adam Wentz <adam@adamwentz.com>
2021-12-11 15:02:35 -0800

    use same masking in text render callback

--- holoformer/models/callbacks/mlm.py
+++ holoformer/models/callbacks/mlm.py
@@ -13,9 +13,24 @@ class EchoMLMTextBatch(Callback):
             return
 
         all_tokens = batch['input_ids'].to(model.device)
-        mask = torch.rand(*all_tokens.shape, device=model.device) < 0.15
-        masked_tokens = all_tokens.clone()
-        masked_tokens[mask] = model.mask_token_id
+        masked_tokens, _ = model.mask_tokens(all_tokens)
         recon_tokens = model(masked_tokens)[0]
+        recon_tokens = torch.argmax(recon_tokens, dim=-1)
+        print(model.tokenizer.decode(recon_tokens))
+
+
+class EchoMLMReducedTextBatch(Callback):
+    def __init__(self, p_print=0.01, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.p_print = p_print
+
+    def on_train_batch_end(self, trainer, model, outputs, batch, *args, **kwargs):
+        if np.random.uniform() > self.p_print:
+            return
+
+        all_tokens = batch['input_ids'].to(model.device)
+        masked_tokens, _ = model.mask_tokens(all_tokens)
+        recon_tokens = model(masked_tokens)[0]
+        # TODO: decode from reduced rep?
         recon_tokens = torch.argmax(recon_tokens, dim=-1)
         print(model.tokenizer.decode(recon_tokens))
