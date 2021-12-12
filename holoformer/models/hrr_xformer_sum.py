f3e8211b2b11be27916496d95a52b2789ac19f87
Adam Wentz <adam@adamwentz.com>
2021-12-11 17:48:31 -0800

    Normalize embedding lookup

--- holoformer/models/hrr_xformer_sum.py
+++ holoformer/models/hrr_xformer_sum.py
@@ -161,6 +161,7 @@ class HoloformerMLM(pl.LightningModule):
     def lookup_embeddings(self, values):
         e = self.embedding.weight.T
         scores = torch.matmul(values, e)
+        scores /= (torch.norm(scores, dim=-1, keepdim=True) + 1e-8)
         return scores
 
     def training_step(self, batch, batch_idx):
