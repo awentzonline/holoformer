0716a9072c6f0c340e18f3dafc6096c3e2fff29e
Adam Wentz <adam@adamwentz.com>
2022-07-07 18:02:32 -0700

    use presence embeddings in hrr_loss

--- holoformer/models/hrr_loss.py
+++ holoformer/models/hrr_loss.py
@@ -1,16 +1,20 @@
 import torch
 
+from holoformer.models import hrr
 
-def hrr_xml_loss(pred, target, embedding):
+
+def hrr_xml_loss(pred, target, embedding, p, m):
     pred = pred / (torch.norm(pred, dim=-1, keepdim=True) + 1e-8)
     target_embs = embedding(target)
     all_embs = embedding.weight.sum(0)
     neg_embs = all_embs - target_embs
     neg_embs = neg_embs / (torch.norm(neg_embs, dim=-1, keepdim=True) + 1e-8)
-    cos_absent = torch.einsum('bi,bi->b', pred, neg_embs)
+    m_pred = hrr.unbind(pred, m)
+    cos_absent = torch.einsum('bi,bi->b', m_pred, neg_embs)
     J_n = torch.mean(torch.abs(cos_absent))
 
     target_embs = target_embs / (torch.norm(target_embs, dim=-1, keepdim=True) + 1e-8)
-    cos_present = torch.einsum('bi,bi->b', pred, target_embs)
+    p_pred = hrr.unbind(pred, p)
+    cos_present = torch.einsum('bi,bi->b', p_pred, target_embs)
     J_p = torch.mean(1 - torch.abs(cos_present))
     return J_p, J_n
